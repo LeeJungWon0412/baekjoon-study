5ebfda584a9a5791805e7d8c09a99ff17a891e48
LeeJungWon0412 <ljw171417@naver.com>
2025-04-30 10:47:52 +0900

    feat: 10811 바구니 뒤집기 문제 풀이

---
+++ baekjoon/4단계/10811.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+arr = list(range(1, n + 1))
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    arr[i - 1:j] = reversed(arr[i - 1:j])
+
+for i in range(n):
+    print(arr[i], end = " ")
